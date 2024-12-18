019735a042e94eeab80c361bc164699b2889db04
Vivek Kumar Singh <82748553+vivek081202@users.noreply.github.com>
2024-12-18 23:11:31 +0530

    Update index.py

--- app/index.py
+++ app/index.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.logo(link="../Images/menu.png")
+st.logo(link="../Images/menu.png", icon_image=LOGO_URL_SMALL)
 
 pages = {
     "App Navigations": [
